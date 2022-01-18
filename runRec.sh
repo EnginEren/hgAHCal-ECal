595a7a2d73105795e6244dace4fa0a43e1ac23f4
Engin Eren <engin.eren@desy.de>
2022-01-18 13:12:10 +0100

    fixigin IO errors

--- runRec.sh
+++ runRec.sh
@@ -4,7 +4,7 @@
 source /home/init_ilcsoft.sh
 source /spack/share/spack/setup-env.sh
 
-export REC_MODEL=ILD_l5_o1_v02
+export REC_MODEL=ILD_sl5_v02
 export MAIN=$(echo $PWD)
 
 git clone --branch v02-02 https://github.com/iLCSoft/ILDConfig.git
